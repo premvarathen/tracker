ada3e5a41ee6ec864aec6ace444173c132862d18
premvarathen <premvarathen@premvarthensmbp>
2018-01-20 21:34:13 -0600

    secod commit

--- src/main/java/com/tracker/boot/main/RestTrackerApplication.java
+++ src/main/java/com/tracker/boot/main/RestTrackerApplication.java
@@ -1,5 +1,7 @@
 package com.tracker.boot.main;
-
+/**
+ * Prem
+ */
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
